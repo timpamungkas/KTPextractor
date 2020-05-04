f314a0c9f301eb92c50638837d492a1a6704ea3c
timpamungkas <timotius.pamungkas@gmail.com>
2020-05-04 17:14:52 +0700

    API extractor

---
+++ KTPextractor_api.py
@@ -0,0 +1,46 @@
+import os
+import uuid
+import urllib.request
+import ocr_text_extractor as ocr
+import ktp_entity_extractor as extractor
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = 'd:\\UPLOADS'
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+@app.route('/ping', methods=['POST'])
+@app.route('/health')
+def hello_world():
+   return 'OK'
+
+@app.route('/ktp', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file uploaded', 400
+	
+    file = request.files['file']
+    
+    if file.filename == '':
+        return 'No file uploaded', 400
+    if file and allowed_file(file.filename):
+        # temporary file
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], str(uuid.uuid1()) + '.' + file.filename.rsplit('.', 1)[1].lower())
+        file.save(filename)
+        ocr.process_ocr(filename)
+        extracted = extractor.process_extract_entities(ocr_path)
+
+        return extracted
+    else:
+        return 'Allowed file types are ' + str(ALLOWED_EXTENSIONS), 400
+
+if __name__ == '__main__':
+    app.run(port=9090)
